12b951374b27f8184ce103138b0245ec1bf816c9
Markus Nilsson <miono@users.noreply.github.com>
2019-02-23 19:35:22 +0100

    safd

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,4 +11,4 @@ add-apt-repository -y ppa:ansible/ansible
 apt-get update
 apt install -y ansible
 
-ansible-playbook install-laptop.yml -e username=$SUDO_USER
+sudo -u $SUDO_USER ansible-playbook install-laptop.yml -kKb -e username=$SUDO_USER
